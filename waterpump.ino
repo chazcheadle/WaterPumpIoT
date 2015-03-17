b938d4d880a7ac0ae41379a5553dfb62d3f87f6b
Chaz Cheadle <ccheadle@gmail.com>
2015-03-17 13:25:13 -0400

    Rewrote the main loop function. Added calibration function. Added checkMotion function.

--- waterpump.ino
+++ waterpump.ino
@@ -1,4 +1,4 @@
-/*
+/**
  * Water Pump Run Time
  *
  * This sketch is for indicating when a water pump starts
@@ -6,32 +6,66 @@
  */
 
 // These constants won't change:
-const int sensorPin = A2;       // Accelerometer pin
+const int xPin = A2;            // Accelerometer X pin
+const int yPin = A3;            // Accelerometer Y pin
 const int indicatorLedPin = 13; // Activity indicator
+const int buttonPin = 12;       // Button pin
 
-// These variables will change:
-int sensorMin = 1023;  // minimum sensor value
-int sensorMax = 0;     // maximum sensor value
-int sensorValue = 0;         // the sensor value
+// Accelerometer configuration:
+// TODO: Maybe use trim pots to adjust sensitivity.
+int xMin = 0;          // minimum X value
+int xMax = 1024;       // maximum X value
+int yMin = 0;          // minimum Y value
+int yMax = 1024;       // maximum Y value
+int activityThreshold = 500; // 500 milliseconds
 
 void setup() {
   // set the LED pins as outputs and the sensor pin as input:
   pinMode(indicatorLedPin, OUTPUT);
-  pinMode (sensorPin, INPUT);
+  pinMode(xPin, INPUT);
+  pinMode(yPin, INPUT);
+  pinMode(buttonPin, INPUT);
+  delay(1000);
+  calibrateSensor();
+}
+
+// Calibrate the sensor.
+void calibrateSensor() {
+  // Allow sensor to settle.
+  delay(1000);
+  // blink 3 times.
+  xMin = analogeRead(xPin);
+  yMin = analogeRead(yPin);
+}
+
+// Check for sustained activity.
+boolean checkMotion() {
+  boolean motion=false;
+  if (analogeRead(xPin) > xMin || analogRead(yPin) > yMin) {
+    motion=true;
+    return motion;
+  }
 }
 
 void loop() {
-  // while the sensor is vibrating, set state to ON:
-  if (analogRead(sensorPin) > sensorMin) {
-    // Wait 500ms to ensure pump is running
-    delay(500);
-    while (analogRead(sensorPin) > sensorMin) {
+  // Recalibrate the sensor.
+  if (analogRead(buttonPin) > 500) {
+    calibrateSensor();
+  }
+
+  if (checkMotion()) {
+    delay(activityThreshold);
+    // Check to see if pump is actually running.
+    if (checkMotion())
+      Serial.print("Water pump turned ON");
       digitalWrite(indicatorLedPin, HIGH);
-      Serial.send(‘Water pump ON’);
-      // Continue checking if sensor is triggered
-      delay(500);
+      // Continue to check pump state.
+      do {
+        delay(50);
+      } while (checkMotion());
+      // When pump stops running, send a notification.
+      Serial.print("Water pump turned OFF");
+      digitalWrite(indicatorLedPin, LOW);
     }
-    digitalWrite(indicatorLedPin, LOW);
-    Serial.send(‘Water pump OFF’);
   }
 }
